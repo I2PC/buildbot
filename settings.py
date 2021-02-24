336e02f392d4a17ae189af9723a241d4c13ba569
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2021-02-24 15:13:22 +0100

    * Changing chimera binary

--- settings.py
+++ settings.py
@@ -110,7 +110,7 @@ except ImportError:
     CRYOLO_CUDA_LIB = "/usr/local/cuda-10.2/lib64"
     RELION_CUDA_LIB = "/usr/local/cuda-10.2/lib64"
     RELION_CUDA_BIN = "/usr/local/cuda-10.2/bin"
-    CHIMERA_HOME = "/home/buildbot/chimerax-1.0"
+    CHIMERA_HOME = "/home/buildbot/chimerax-1.1"
     EMAN2_HOME = os.path.join(EM_ROOT, 'eman-3.0.0-alpha')
     JJSOFT_HOME = "/home/buildbot/scipionBot/jjsoft"
     BUILD_TESTS = "True"
