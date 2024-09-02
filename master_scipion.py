c33f4c96b93c9868305a95b263165defe1ba9559
Yunior C. Fonseca Reyna <cfonsecareyna82@gmail.com>
2024-09-02 18:38:11 +0200

    installing xmipp

--- master_scipion.py
+++ master_scipion.py
@@ -780,7 +780,7 @@ def installSDevelScipionFactory(groupId):
         haltOnFailure=True))
 
     # Clone and Install Xmipp
-    installXmippCmd = 'git clone https://github.com/I2PC/xmipp.git && xmipp/xmipp getSources -b devel && ./scipion3 installp -p xmipp/src/scipion-em-xmipp --devel'
+    installXmippCmd = 'rm -rf xmipp && git clone https://github.com/I2PC/xmipp.git && xmipp/xmipp getSources -b devel && ./scipion3 installp -p xmipp/src/scipion-em-xmipp --devel'
     compileXmippCmd = './scipion3 run xmipp/xmipp'
 
     installScipionFactorySteps.addStep(ScipionCommandStep(
