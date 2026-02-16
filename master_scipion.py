da64069e3bfd29f2bf16c4a4e943964cebe994fc
Yunior C. Fonseca Reyna <cfonsecareyna82@gmail.com>
2026-02-16 10:01:54 +0100

    Using pyxmipp3 to install scipion+xmipp

--- master_scipion.py
+++ master_scipion.py
@@ -778,15 +778,15 @@ def installSDevelScipionFactory(groupId):
         haltOnFailure=True))
 
     # Clone and Install Xmipp
-    installXmippCmd = 'rm -rf xmipp3 && rm -rf scipion-em-xmipp && git clone https://github.com/I2PC/xmipp3.git && ./scipion3 run pip install xmipp3-installer && ./scipion3 run xmipp3/xmipp  &&  cd xmipp3/src && git clone https://github.com/I2PC/scipion-em-xmipp.git && ../../scipion3 installp -p scipion-em-xmipp --devel'
+    installXmippCmd = 'rm -rf xmipp3 && rm -rf scipion-em-xmipp && git clone https://github.com/I2PC/xmipp3.git && ./scipion3 run pip install xmipp3-installer && cd xmipp3 && git checkout pyxmipp && cd .. && ./scipion3 run xmipp3/xmipp  &&  cd xmipp3/src && git clone https://github.com/I2PC/scipion-em-xmipp.git && ../../scipion3 installp -p scipion-em-xmipp --devel'
 
-    # installScipionFactorySteps.addStep(ScipionCommandStep(
-    #     command=installXmippCmd,
-    #     name='Installing Xmipp',
-    #     description='Installing Xmipp',
-    #     descriptionDone='Installing Xmipp',
-    #     timeout=settings.timeOutInstall,
-    #     haltOnFailure=False))
+    installScipionFactorySteps.addStep(ScipionCommandStep(
+        command=installXmippCmd,
+        name='Installing Xmipp',
+        description='Installing Xmipp',
+        descriptionDone='Installing Xmipp',
+        timeout=settings.timeOutInstall,
+        haltOnFailure=False))
 
     # installScipionFactorySteps.addStep(
     #     (ShellCommand(command=['chmod', '777', '-R', settings.SDEVEL_ENV_PATH],
@@ -856,6 +856,16 @@ def installSDevelScipionFactory(groupId):
         name='Install plugin scipion-em-tomo',
         description='Install plugin scipion-em-tomo',
         descriptionDone='Installed plugin scipion-em-tomo',
+        timeout=settings.timeOutInstall,
+        haltOnFailure=True))
+
+    installCmd = (settings.SCIPION_CMD + ' pip install pyxmipp3')
+
+    installScipionFactorySteps.addStep(ScipionCommandStep(
+        command=installCmd,
+        name='Install pyxmipp3',
+        description='Install pyxmipp3',
+        descriptionDone='Installed pyxmipp3',
         timeout=settings.timeOutInstall,
         haltOnFailure=True))
 
