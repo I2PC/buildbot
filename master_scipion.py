33e262f5f8ccc5299e6a804d2de9da90f026afe4
alberto <alberto.garcia@cnb.csic.es>
2026-06-09 14:55:49 +0200

    Update master_scipion.py

--- master_scipion.py
+++ master_scipion.py
@@ -838,7 +838,7 @@ def installSDevelScipionFactory(groupId):
     
     # Clone and Install Xmipp
     #installXmippCmd = 'rm -rf xmipp-bundle && rm -rf scipion-em-xmipp && git clone https://github.com/I2PC/xmipp3.git xmipp-bundle && ./scipion3 run pip install xmipp3-installer && cd xmipp-bundle && git checkout pyxmipp && cd .. && ./scipion3 run xmipp-bundle/xmipp  &&  cd xmipp-bundle/src && git clone https://github.com/I2PC/scipion-em-xmipp.git && cd scipion-em-xmipp && git checkout pyxmipp && ../../../scipion3 installp -p . --devel'
-    installXmippCmd =  'rm -rf xmipp-bundle && rm -rf scipion-em-xmipp && git clone https://github.com/I2PC/xmipp3.git xmipp-bundle && ./scipion3 run pip install xmipp3-installer && ./scipion3 run xmipp-bundle/xmipp  &&  cd xmipp-bundle/src && git clone https://github.com/I2PC/scipion-em-xmipp.git && ../../scipion3 installp -p . --devel'
+    installXmippCmd =  'rm -rf xmipp-bundle && rm -rf scipion-em-xmipp && git clone https://github.com/I2PC/xmipp3.git xmipp-bundle && ./scipion3 run pip install xmipp3-installer && ./scipion3 run xmipp-bundle/xmipp  &&  cd xmipp-bundle/src && git clone https://github.com/I2PC/scipion-em-xmipp.git && ../../scipion3 installp -p scipion-em-xmipp --devel'
     installScipionFactorySteps.addStep(ScipionCommandStep(
         command=installXmippCmd,
         name='Installing Xmipp',
