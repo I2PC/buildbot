f7fed02257b8e8365af768317f61741906b82d11
alberto <alberto.garcia@cnb.csic.es>
2026-06-17 09:33:21 +0200

    changing TESTS path to /data

--- master_scipion.py
+++ master_scipion.py
@@ -108,7 +108,12 @@ setMpiBinPath = ShellCommand(
 
 # Use a common home data tests folder to save storage
 setDataTestsDir = ShellCommand(
-    command=['sed', '-i', '-e', 's|^SCIPION_TESTS\s*=.*|SCIPION_TESTS = /data/buildbot/tests|g' config/scipion.conf],
+    command=[
+        'sed',
+        '-i',
+        '-e',
+        's|^SCIPION_TESTS\\s*=.*|SCIPION_TESTS = /data/buildbot/tests|g',
+        'config/scipion.conf'],
     name='Set data tests dir',
     description='Using a common data tests dir',
     descriptionDone='Change data tests dir',
